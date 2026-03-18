79dc3ae7db93a23da5ba92dc170bddf8eaec8346
lngiri <loknathgirir@gmail.com>
2026-03-18 13:32:31 +0545

    fix: remove password hook causing 500 error on register

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -28,13 +27,6 @@ const userSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   }
-});
-
-// Hash password before saving
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 12);
-  next();
 });
 
 module.exports = mongoose.model('User', userSchema);
